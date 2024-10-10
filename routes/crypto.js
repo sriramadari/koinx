1660806eebfac52f1b5966e074073e734b1072a0
sriramadari <lakshmisriramadari1427@gmail.com>
2024-10-10 05:48:58 +0530

    [fix]: optimized storing data in db and modifed stats api

--- routes/crypto.js
+++ routes/crypto.js
@@ -3,23 +3,36 @@ const Crypto = require('../models/Crypto');
 const router = express.Router();
 
 router.get('/stats', async (req, res) => {
-  const { coin } = req.query;
-
-  try {
-    const latestData = await Crypto.findOne({ coin }).sort({ timestamp: -1 });
-
-    if (!latestData) {
-      return res.status(404).json({ error: 'Data not found' });
+    const { coin } = req.query;
+  
+    if (!coin) {
+      return res.status(400).json({ error: 'Coin is required' });
     }
+  
+    try {
+      const cryptoData = await Crypto.findOne({ coin })
+        .sort({ date: -1 }) 
+        .limit(1); 
+  
+      if (!cryptoData) {
+        return res.status(404).json({ error: 'Data not found for the specified coin' });
+      }
+  
+      const latestPrice = cryptoData.prices[cryptoData.prices.length - 1];
+      const latestMarketCap = cryptoData.marketCaps[cryptoData.marketCaps.length - 1];
+      const latestChange24h = cryptoData.changes24h[cryptoData.changes24h.length - 1];
+  
+      res.json({
+        price: latestPrice,
+        marketCap: latestMarketCap,
+        '24hChange': latestChange24h
+      });
 
-    res.status(200).json({
-      price: latestData.price,
-      marketCap: latestData.marketCap,
-      '24hChange': latestData.change24h
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
+
+    } catch (error) {
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+
 
 module.exports = router;
