8c9dd6abd263c59be7e945e3440f899a330ece5b
sriramadari <lakshmisriramadari1427@gmail.com>
2024-10-10 05:46:16 +0530

    [fix]: modified cron job function

--- services/fetchCryptoData.js
+++ services/fetchCryptoData.js
@@ -3,34 +3,45 @@ const Crypto = require('../models/Crypto');
 
 const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price';
 
-const COINS = ['bitcoin', 'ethereum', 'litecoin']; 
+const COINS = ['bitcoin', 'matic-network', 'ethereum'];
 
 
 const fetchCryptoData = async () => {
   try {
+        const currentDate = new Date().toISOString().split('T')[0]; 
 
-    const { data } = await axios.get(COINGECKO_API_URL, {
-      params: {
-        ids: COINS.join(','),
-        vs_currencies: 'usd',
-        include_market_cap: 'true',
-        include_24hr_change: 'true'
-      }
-    });
-
-    for (const coin of COINS) {
-      const coinData = data[coin];
-      if (coinData) {
-        await Crypto.create({
-          coin,
-          price: coinData.usd,
-          marketCap: coinData.usd_market_cap,
-          change24h: coinData.usd_24h_change
+        const { data } = await axios.get(COINGECKO_API_URL, {
+          params: {
+            ids: COINS.join(','),
+            vs_currencies: 'usd',
+            include_market_cap: 'true',
+            include_24hr_change: 'true'
+          }
         });
-      } else {
-        console.warn(`No data found for coin: ${coin}`);
-      }
-    }
+    
+        for (const coin of COINS) {
+          const coinData = data[coin];
+    
+          if (coinData) {
+            let cryptoRecord = await Crypto.findOne({ coin, date: currentDate });
+    
+            if (!cryptoRecord) {
+              cryptoRecord = new Crypto({
+                coin,
+                date: currentDate,
+                prices: [],
+                marketCaps: [],
+                changes24h: []
+              });
+            }
+    
+            cryptoRecord.prices.push(coinData.usd);
+            cryptoRecord.marketCaps.push(coinData.usd_market_cap);
+            cryptoRecord.changes24h.push(coinData.usd_24h_change);
+    
+            await cryptoRecord.save();
+          }
+        }
     console.log('Crypto data fetched and stored successfully');
   } catch (error) {
     console.error('Error fetching crypto data:', error.message);
